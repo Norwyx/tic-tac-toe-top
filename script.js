49c7df8e4e83475e2acf5801a7028a3c9110d7d0
Manuel Salazar <mannisuso@gmail.com>
2024-04-27 22:01:25 -0500

    Create gameBoard object constructor

--- script.js
+++ script.js
@@ -10,4 +10,24 @@ const Player = (sign) => {
     return { getSign }
 }
 
+const gameBoard = (() => {
+    const board = ["", "", "", "", "", "", "", "", ""]
 
+    const setField = (sign, index) => {
+        if (index > board.length) return 
+        board[index] = sign
+    }
+
+    const getField = (index) => {
+        if (index > board.length) return 
+        return board[index]
+    }
+
+    const reset = () => {
+        for (let i = 0; i < board.length; i++) {
+            board[i] = ""
+        }
+    }
+
+    return { setField, getField, reset }
+})()
